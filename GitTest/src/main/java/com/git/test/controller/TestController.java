c91bcba5661b93c95f17951a0b82886fcdf796a9
pawanjashnani <pawanjashnani@gmail.com>
2015-06-06 19:30:14 +0530

    test

---
+++ GitTest/src/main/java/com/git/test/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.git.test.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping(method=RequestMethod.GET, value="/test.htm")
+	public String Test(HttpServletRequest request, HttpServletResponse response){
+		return "test";
+	}
+
+}
